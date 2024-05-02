35a0973d000125383473adff8ec8765e1de967fe
youngrankwon <youngran.kwon@gsitm.com>
2024-05-02 16:08:37 +0900

    first commit

---
+++ src/main/java/tobyspring/myboot/HelloController.java
@@ -0,0 +1,18 @@
+package tobyspring.myboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello(String name) {
+		return "Hello " + name;
+	}
+
+	@GetMapping("/")
+	public String main() {
+		return "Main";
+	}
+}
